4187ffdbe8ec1c88c1268de4730e4b655c728fe7
RawanAlg <53184591+RawanAlg@users.noreply.github.com>
2019-08-25 14:25:38 +0300

    Add files via upload

---
+++ Day4.py
@@ -0,0 +1,60 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("Hi")
+Hi
+>>> """Today we will learn the python numbers
+i think it is intersting"""
+'Today we will learn the python numbers\ni think it is intersting'
+>>> x=1
+>>> y=3.3
+>>> z=2a
+SyntaxError: invalid syntax
+>>> print (type(x))
+<class 'int'>
+>>> print (type(y))
+<class 'float'>
+>>> print (type(z))
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    print (type(z))
+NameError: name 'z' is not defined
+>>> z=2 a
+SyntaxError: invalid syntax
+>>> z= 3j
+>>> print (type(z))
+<class 'complex'>
+>>> #note: space needed
+>>> a= float(x)
+>>> b= int(y)
+>>> c= complex(x)
+>>> print(a)
+1.0
+>>> print (type(x))
+<class 'int'>
+>>> print (type(a))
+<class 'float'>
+>>> print (type(b))
+<class 'int'>
+>>> print (type(c))
+<class 'complex'>
+>>> 
+>>> import random
+>>> print (random.randrange(10,33))
+31
+>>> print (random.randrange(10,33))
+16
+>>> print (random.randrange(10,33))
+27
+>>> print (random.randrange(10,33))
+30
+>>> print (random.randrange(10,33))
+31
+>>> print (random.randrange(10,33))
+12
+>>> print (random.randrange(10,33))
+12
+>>> print (random.randrange(10,33))
+17
+>>> #amazing.
+>>> #Day4ended.
+>>> 
